f2d63413eaf32c7a0ad7044e5e576fef18e02985
Sarthak Batra <sarthakbatra1993@gmail.com>
2019-05-21 10:10:15 +0400

    Initialized list of squads

---
+++ data/squads.py
@@ -0,0 +1,4 @@
+india_squad = ['Virat Kohli', 'Rohit Sharma', 'Shikhar Dhawan']
+west_indies_squad = ['Chris Gayle', 'Jason Holder', 'Andre Russell']
+
+all_squads = [india_squad, west_indies_squad]
