fd2e38979d8670d78858c1e150d6458130523738
Sarthak Batra <sarthakbatra1993@gmail.com>
2019-05-21 10:09:59 +0400

    Nested loop implementation

--- data/download_data.py
+++ data/download_data.py
@@ -1,18 +1,19 @@
 # https://github.com/hardikvasa/google-images-download
 from google_images_download import google_images_download
+from squads import all_squads
 
 response = google_images_download.googleimagesdownload()
 
-india_squad = ['Virat Kohli', 'Rohit Sharma', 'Shikhar Dhawan']
+for squad in all_squads:
+    for player in squad:
 
-for player in india_squad:
+        arguments = {
+            'keywords': player + ' cricketer',
+            'limit': 5,
+            'print_urls': True,
+            'image_directory': player
+            # 'no_download': 'nd'
+            }
 
-    arguments = {
-        'keywords': player,
-        'limit': 5,
-        'print_urls': True,
-        # 'no_download': 'nd'
-        }
-
-    absolute_image_paths = response.download(arguments)
-    print(arguments)
+        absolute_image_paths = response.download(arguments)
+        # print(arguments)
