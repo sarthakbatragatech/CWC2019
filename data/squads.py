6f11d8f5b676ffc4a7b199a2820103515091ab84
Sarthak Batra <sarthakbatra1993@gmail.com>
2019-05-21 12:20:48 +0400

    Created latest squads

--- data/squads.py
+++ data/squads.py
@@ -1,4 +1,85 @@
-india_squad = ['Virat Kohli', 'Rohit Sharma', 'Shikhar Dhawan']
-west_indies_squad = ['Chris Gayle', 'Jason Holder', 'Andre Russell']
+afghanistan = [
+    'Noor Ali Zadran', 'Hazratullah Zazai', 'Asghar Afghan',
+    'Hashmatullah Shahidi', 'Najibullah Zadran', 'Rashid Khan',
+    'Dawlat Zadran', 'Aftab Alam', 'Hamid Hassan',
+    'Mujeeb Ur Rahman', 'Gulbadin Naib', 'Rahmat Shah',
+    'Samiullah Shenwari', 'Mohammad Nabi', 'Mohammad Shahzad',
+]
 
-all_squads = [india_squad, west_indies_squad]
+australia = [
+    'Aaron Finch', 'Usman Khawaja', 'David Warner',
+    'Steve Smith', 'Shaun Marsh', 'Glenn Maxwell',
+    'Marcus Stoinis', 'Alex Carey', 'Pat Cummins',
+    'Mitchell Starc', 'Kane Richardson', 'Nathan Coulter-Nile',
+    'Jason Behrendorff', 'Nathan Lyon', 'Adam Zampa',
+]
+
+bangladesh = [
+    'Mashrafe Mortaza', 'Tamim Iqbal', 'Liton Das',
+    'Soumya Sarkar', 'Mushfiqur Rahim', 'Mahmudullah',
+    'Shakib Al Hasan', 'Mohammad Mithun', 'Sabbir Rahaman',
+    'Mosaddek Hossain', 'Mohammad Saifuddin', 'Mehidy Hasan Miraz',
+    'Rubel Hossain', 'Mustafizur Rahman', 'Abu Jayed',
+]
+
+england = [
+    'Jonny Bairstow', 'Jason Roy', 'Joe Root',
+    'Eoin Morgan', 'Ben Stokes', 'Jos Buttler',
+    'Moeen Ali', 'Chris Woakes', 'Liam Plunkett',
+    'Adil Rashid', 'Mark Wood', 'Tom Curran',
+    'Liam Dawson', 'Jofra Archer', 'James Vince',
+]
+
+india = [
+     'Virat Kohli', 'Rohit Sharma', 'Shikhar Dhawan',
+     'KL Rahul', 'Vijay Shankar', 'MS Dhoni',
+     'Kedar Jadhav', 'Dinesh Karthik', 'Yuzvendra Chahal',
+     'Kuldeep Yadav', 'Bhuvneshwar Kumar', 'Jasprit Bumrah',
+     'Hardik Pandya', 'Ravindra Jadeja', 'Mohammed Shami',
+]
+
+pakistan = [
+    'Sarfaraz Ahmed', 'Asif Ali', 'Babar Azam',
+    'Fakhar Zaman', 'Haris Sohail', 'Hasan Ali',
+    'Imad Wasim', 'Imam-ul-Haq', 'Mohammad Amir',
+    'Mohammad Hafeez', 'Mohammad Hasnain', 'Shadab Khan',
+    'Shaheen Afridi', 'Shoaib Malik', 'Wahab Riaz',
+]
+
+south_africa = [
+    'Faf du Plessis', 'JP Duminy', 'David Miller',
+    'Dale Steyn', 'Andile Phehlukwayo', 'Imran Tahir',
+    'Hashim Amla', 'Kagiso Rabada', 'Dwaine Pretorius',
+    'Quinton de Kock', 'Chris Morris', 'Lungi Ngidi',
+    'Aiden Markram', 'Rassie vd Dussen', 'Tabraiz Shamsi',
+]
+
+srilanka = [
+    'Dimuth Karunaratne', 'Lasith Malinga', 'Angelo Mathews',
+    'Thisara Perera', 'Kusal Perera', 'Dhananjaya de Silva',
+    'Kusal Mendis', 'Isuru Udana', 'Milinda Siriwardana',
+    'Avishka Fernando', 'Jeevan Mendis', 'Lahiru Thirimanne',
+    'Jeffrey Vandersay', 'Nuwan Pradeep', 'Suranga Lakmal',
+]
+
+west_indies = [
+    'Jason Holder', 'Andre Russell', 'Ashley Nurse',
+    'Carlos Brathwaite', 'Chris Gayle', 'Darren Bravo',
+    'Evin Lewis', 'Fabian Allen', 'Kemar Roach',
+    'Nicholas Pooran', 'Oshane Thomas', 'Shai Hope',
+    'Shannon Gabriel', 'Sheldon Cottrell', 'Shimron Hetmyer',
+]
+
+new_zealand = [
+    'Kane Williamson', 'Tom Blundell', 'Trent Boult',
+    'Colin de Grandhomme', 'Lockie Ferguson', 'Martin Guptill',
+    'Matt Henry', 'Tom Latham', 'Colin Munro',
+    'Jimmy Neesham', 'Henry Nicholls', 'Mitchell Santner',
+    'Ish Sodhi', 'Tim Southee', 'Ross Taylor',
+]
+
+all_squads = [
+    afghanistan, australia, bangladesh, england,
+    india, new_zealand, pakistan, south_africa,
+    srilanka, west_indies,
+    ]
